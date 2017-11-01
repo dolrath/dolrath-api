b41e11b4c21000d5bf4b800ce9158f57c4c992b0
Eric O Freitas <erics9@gmail.com>
2017-11-01 12:57:38 -0700

    Correção dos dados

--- app.js
+++ app.js
@@ -9,17 +9,12 @@ app.use('/client', express.static(__dirname + '/client'));
 
 serv.listen(8080);
 
-//rolar dado
-//function rollDice(x){
-//    return(round(Math.random*x));
-//}
-
 var io = require('socket.io')(serv, {});
 io.sockets.on('connection', function(socket){
     console.log('Socket funfando');
 
     socket.on('rollDice', function(data){
-        console.log('Rolou d4 e tirou '+ round(Math.random*data.dice));
+        console.log('Rolou d'+data.dice+' e tirou '+ Math.round(Math.random()*data.dice));
     })
 
 
